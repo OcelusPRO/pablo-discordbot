2c5eacaa0313b390da12f660563bb77ca87f46b2
OcelusPRO <ocelus1999@hotmail.com>
2022-09-07 21:18:49 +0200

    remove unused imports

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,4 @@
-const { Client } = require("discord.js");
-const {ICmd} = require("./core/commands/ICmd");
-const glob = require("glob");
 const Main = require("./Main");
-
 
 const main = new Main(process.env.PABLO_BOT_TOKEN)
 main.login()
